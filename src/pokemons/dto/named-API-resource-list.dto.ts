545e96612a07742c89b4c993082f1d310ced0612
Samuel Clara <clara.samuel@supelec.fr>
2022-09-13 08:07:20 +0200

    First step with httpModule

---
+++ src/pokemons/dto/named-API-resource-list.dto.ts
@@ -0,0 +1,16 @@
+import {ApiPropertyOptional} from "@nestjs/swagger";
+import {NamedAPIResourceDto} from "./named-API-resource.dto";
+
+export class NamedAPIResourceListDto {
+    @ApiPropertyOptional({ type: Number })
+    readonly count: number;
+
+    @ApiPropertyOptional({ type: String })
+    readonly next: string;
+
+    @ApiPropertyOptional({ type: String })
+    readonly previous: string;
+
+    @ApiPropertyOptional({ type: [NamedAPIResourceDto] })
+    readonly results: NamedAPIResourceDto[];
+}
--- src/pokemons/pokemons.service.ts
+++ src/pokemons/pokemons.service.ts
@@ -1,12 +1,21 @@
 import {Injectable, Logger} from '@nestjs/common';
+import {HttpService} from "@nestjs/axios";
+import {NamedAPIResourceListDto} from "./dto/named-API-resource-list.dto";
+import {map} from "rxjs/operators";
+import {Observable} from "rxjs";
+
+const URL_API_POKEMON = "https://pokeapi.co/api/v2/pokemon";
 
 @Injectable()
 export class PokemonsService {
     private readonly logger = new Logger(PokemonsService.name);
 
-    getPokemons(pageNumber: number = 0): string {
+    constructor(private readonly httpService: HttpService) {}
+
+    async getPokemons(pageNumber: number = 0): Promise<Observable<NamedAPIResourceListDto>>{
         this.logger.log('Returning pokemon from page : ' + pageNumber);
-        return 'Pokemons - page : ' + pageNumber;
+        return await this.httpService.get<NamedAPIResourceListDto>(URL_API_POKEMON)
+            .pipe(map(response => response.data))
     }
 
     getPokemon(pokemonID : number) : string{
